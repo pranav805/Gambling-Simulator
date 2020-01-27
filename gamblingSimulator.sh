30551a2f3dcd00091094615e397f2192d2c52d4a
pranav805 <pranav.ige96@gmail.com>
2020-01-27 12:52:20 +0530

    ADDED: while statement

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -x
 echo "Welcome to Gambling Simulator"
+
 #CONSTANTS
 BET=1
 
 #VARIABLES
 stake=100
 
-if [ $((RANDOM%2)) -eq 1 ]
+while [[ $stake -ne 50 && $stake -ne 150 ]]
+do
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		stake=$(($stake+$BET))
+	else
+		stake=$(($stake-$BET))
+	fi
+done
+
+if [ $stake -eq 50 ]
 then
-	echo Won
-	stake=$(($stake+$BET))
+	echo "You have LOST 50% of your stake"
 else
-	echo Lost
-	stake=$(($stake-$BET))
+	echo "You have WON 50% of your stake"
 fi
