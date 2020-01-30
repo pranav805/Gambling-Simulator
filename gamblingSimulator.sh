339077b8a149b220a0d669c1c932e46749b4cca7
pranav805 <pranav.ige96@gmail.com>
2020-01-30 15:53:19 +0530

    ADDED:if won,then continue playing or not. MODIFIED:moved pieces of codes into functions

--- gamblingSimulator.sh
+++ gamblingSimulator.sh
@@ -1,59 +1,81 @@
 #!/bin/bash
 echo "Welcome to Gambling Simulator"
-
-#CONSTANTS
-BET=1
-
+echo 
 #VARIABLES
 stake=100
 winningAmount=0
-looseingAmount=0
+loosingAmount=0
 winDays=0
 loseDays=0
-presentAmount=0
+amountInHand=0
+
+#CONSTANTS
+BET=1
+NO_OF_DAYS=20
+PERCENTAGE=50
+WIN=$(($(($PERCENTAGE*$stake/100)) + $stake))
+LOSE=$(($stake - $(($PERCENTAGE*$stake/100))))
 
 declare -A luckyOrUnluckyDay
-for((i=1;i<=20;i++))
-do
-	while [[ $stake -ne 50 && $stake -ne 150 ]]
+declare -A gamblerWinOrLose
+
+function betWonOrLost(){
+	if [ $((RANDOM%2)) -eq 1 ]
+	then
+		stake=$(($stake + $BET))
+  	else
+		stake=$(($stake - $BET))
+	fi
+}
+
+function wonOrLose(){
+   if [ $stake -eq $LOSE ]
+   then
+      loosingAmount=$(($loosingAmount + 50))
+		((loseDays++))
+      stake=100
+      amountInHand=$(($amountInHand - 50))
+      gamblerWinOrLose[$1]="LOST"
+   else
+      winningAmount=$(($winningAmount + 50))
+		((winDays++))
+      stake=100
+		amountInHand=$(($amountInHand + 50))
+      gamblerWinOrLose[$1]="WON"
+   fi
+}
+
+function gamblingGame(){
+	for((i=1;i<=$NO_OF_DAYS;i++))
 	do
-		if [ $((RANDOM%2)) -eq 1 ]
-		then
-			stake=$(($stake+$BET))
-		else
-			stake=$(($stake-$BET))
-		fi
+		while [[ $stake -ne $LOSE && $stake -ne $WIN ]]
+		do
+			betWonOrLost
+		done
+		wonOrLose $i
+		luckyOrUnluckyDay[$i]=$amountInHand
 	done
 
-	if [ $stake -eq 50 ]
+	if [ $winningAmount -gt $loosingAmount ]
 	then
-		loosingAmount=$(($loosingAmount + 50))
-		((loseDays++))
-		stake=100
-		presentAmount=$(($presentAmount - 50))
+		echo "After 20 days of playing, you WON ₹$(($winningAmount - $loosingAmount))"
+	elif [ $winningAmount -lt $loosingAmount ]
+	then
+		echo "After 20 days of playing, you LOST ₹$(($loosingAmount - $winningAmount))"
 	else
-		winningAmount=$(($winningAmount + 50))
-		((winDays++))
-		stake=100
-		presentAmount=$(($presentAmount + 50))
+		echo "You neither lose nor win"
 	fi
-	luckyOrUnluckyDay[$i]=$presentAmount
-done
-
-if [ $winningAmount -gt $loosingAmount ]
-then
-	echo "After 20 days of playing, you WON ₹$(($winningAmount - $loosingAmount))"
-elif [ $winningAmount -lt $loosingAmount ]
-then
-	echo "After 20 days of playing, you LOST ₹$(($loosingAmount - $winningAmount))"
-else
-	echo "You neither lose nor win"
-fi
 
 echo Number of days of WON: $winDays
 echo Number of days of LOST: $loseDays
 echo Total amount WON: $winningAmount
 echo Total amount LOST: $loosingAmount
+echo
+echo "Days: 	   ${!luckyOrUnluckyDay[@]}"
+echo "Amount:	   ${luckyOrUnluckyDay[@]}"
+echo "Won/Lost:  ${gamblerWinOrLose[@]}"
+echo
+}
 
 function luckyOrNot(){
 for i in ${!luckyOrUnluckyDay[@]}
@@ -62,7 +84,26 @@ do
 done | sort -k2 $1 | head -1
 }
 
-echo "Luckiest Day and Amount Won Maximum: " 
-luckyOrNot -rn
-echo "Unluckiest Day and Amount Lost Maximum: "
-luckyOrNot -n
+while [ $amountInHand -ge 0 ]
+do
+	gamblingGame
+	echo "Luckiest Day and amount Won Maximum:"
+	luckyOrNot -rn
+	echo "Unluckiest Day and amount Lost Maximum:"
+	luckyOrNot -n
+	if [ ${luckyOrUnluckyDay[20]} -ge 0 ]
+	then
+		read -p "Do you want to continue for next month [y/n]: " result
+		if [ $result == "y" ]
+		then
+			echo "---Next month---"
+			winningAmount=0
+			loosingAmount=0
+			winDays=0
+			loseDays=0
+			amountInHand=0
+		else
+			break
+		fi
+	fi
+done
